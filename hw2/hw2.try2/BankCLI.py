b11836ee900e5f73601d695ac06a130912d9d9ad
ading01 <allan.ding@yale.edu>
2022-03-23 23:39:37 +0100

    upload hw3

---
+++ hw2/hw2.try2/BankCLI.py
@@ -0,0 +1,177 @@
+import sys
+import pickle
+
+from datetime import datetime
+from Bank import Bank
+from Transactions import Transaction
+from Accounts import OverdrawError
+from Accounts import TransactionLimitError, TransactionSequenceError
+from decimal import Decimal, DecimalException
+import logging
+
+logging.basicConfig(filename='bank.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+# -----------------BEFORE SUBMITTING-------------------------
+# write error handling if a type of account is not checking or saving
+# what if you try to open an account with a negative
+# what happens when you don't enter a valid account number?
+# what happens if you try to select an account and there are no accounts?
+# trying to apply fees without an account selected
+
+class BankCLI():
+    def __init__(self):
+        self._bank = Bank()
+        self._selected_account = None
+        self._choices = {
+            "1": self._open_account,
+            "2": self._summary,
+            "3": self._select,
+            "4": self._list_transactions,
+            "5": self._add_transaction,
+            "6": self._monthy_triggers,
+            "7": self._save,
+            "8": self._load,
+            "9": self._quit,
+        }
+
+
+    def _display_menu(self):
+        print(f"""--------------------------------
+Currently selected account: {self._selected_account}
+Enter command
+1: open account
+2: summary
+3: select account
+4: list transactions
+5: add transaction
+6: interest and fees
+7: save
+8: load
+9: quit""")
+
+    def run(self):        
+        """Display the menu and respond to choices."""
+
+        while True:
+            self._display_menu()
+            choice = input(">")
+            action = self._choices.get(choice)
+            if action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choice))
+
+
+    def _summary(self):
+        for x in self._bank.show_accounts():
+            print(x)
+
+    def _load(self):
+        with open("save.pickle", "rb") as f:
+            self._bank = pickle.load(f)
+        logging.debug("Loaded from bank.pickle")
+        
+
+    def _save(self):
+        with open("save.pickle", "wb") as f:
+            pickle.dump(self._bank, f)
+        logging.debug("Saved to bank.pickle")
+
+    def _quit(self):
+        sys.exit(0)
+
+    def _add_transaction(self):    
+        if self._selected_account == None:
+            print("This command requires that you first select an account.")
+            return
+
+        while True:
+            try:
+                amount = Decimal(input("Amount?\n>"))
+                break
+            except (ValueError, DecimalException):
+                print("Please try again with a valid dollar amount.")
+            
+        while True:
+            try:
+                date = input("Date? (YYYY-MM-DD)\n>")
+                datetime.strptime(date, "%Y-%m-%d")
+                break
+            except ValueError:
+                print("Please try again with a valid date in the format YYYY-MM-DD.")
+
+        t = Transaction(amount, date)
+        logging.debug(f"Created transaction: {self._selected_account.get_acct_num()}, {amount}")
+        try:
+            self._selected_account.add_transaction(t)
+            # print(1)
+        except AttributeError:
+            print("This command requires that you first select an account.")
+        except OverdrawError:
+            # print(2)
+            print("This transaction could not be completed due to an insufficient account balance.")
+        except TransactionLimitError:
+            print("This transaction could not be completed because the account has reached a transaction limit.")
+        except TransactionSequenceError as e:
+            print("New transactions must be from {0} onward.".format(e))
+        logging.debug(f"Created transaction: {self._selected_account.get_acct_num()}, {t.get_amount()}")
+
+
+    def _open_account(self):
+        while True:
+            acct_type = input("Type of account? (checking/savings)\n>")
+            if acct_type == "checking" or acct_type == "savings":
+                break
+            else:
+                print("Please try again and enter either 'checking' or 'savings'")
+
+        while True:
+            try:
+                initial_deposit = Decimal(input("Initial deposit amount?\n>"))
+                break
+            except (ValueError, DecimalException):
+                print("Please try again with a valid dollar amount.")
+
+        
+
+        t = Transaction(initial_deposit)
+
+        try: 
+            a = self._bank.add_account(acct_type)
+            a.add_transaction(t)
+        except OverdrawError:
+            print("Cannot open an account with negative value")
+        
+        
+    def _select(self):
+        num = int(input("Enter account number\n>"))
+        self._selected_account = self._bank.get_account(num)
+
+    def _monthy_triggers(self):
+        try:
+            self._selected_account.assess_interest_and_fees()
+        except TransactionSequenceError as e:
+            month = datetime.strftime(e.latest_date, "%B")
+            print(f"Cannot apply interest and fees again in the month of {month}.")
+        except AttributeError:
+            print("This command requires that you first select an account.")
+        logging.debug("Triggered fees and interest")
+            
+
+    def _list_transactions(self):
+        try: 
+            for x in self._selected_account.get_transactions():
+                print(x)
+        except AttributeError:
+            print("This command requires that you first select an account.")
+            
+
+
+
+
+if __name__ == "__main__":
+    BankCLI().run()
+    try:
+        BankCLI().run()
+    except Exception:
+        print("Sorry! Something unexpected happened. If this problem persists please contact our support team for assistance.")
---
+++ hw2/tryagain/113950182/Accounts.py
@@ -0,0 +1,173 @@
+from logging import raiseExceptions
+import logging
+from unittest.mock import Base
+from Transactions import Transaction
+from decimal import Decimal
+import calendar
+
+class OverdrawError(Exception):
+    pass
+
+class TransactionLimitError(Exception):
+    pass
+
+class TransactionSequenceError(Exception):
+    def __init__(self, latest_date):
+        self.latest_date = latest_date
+        
+logging.basicConfig(filename='bank.log', level=logging.DEBUG, format='%(asctime)s|%(levelname)s|%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+
+class Account:
+    """This is an abstract class for accounts.  Provides default functionality for adding transactions, getting balances, and assessing interest and fees.  
+    Accounts should be instantiated as SavingsAccounts or CheckingAccounts
+    """
+
+    def __init__(self, acct_num):
+        self._transactions = []
+        self._account_number = acct_num
+        self.last_exempt = None
+        self.latest_date = None # should be of type date
+        logging.debug(f"Created account: {self._account_number}")
+
+    def get_acct_num(self):
+        return self._account_number
+
+
+    def add_transaction(self, amt, date=None, exempt=False):
+        """Checks a pending transaction to see if it is allowed and adds it to the account if it is.
+
+        Args:
+            t (Transaction): incoming transaction
+        """
+        
+        t = Transaction(amt,
+                        self._account_number,
+                        date=date, 
+                        exempt=exempt)
+
+        if self.latest_date == None:
+            self.latest_date = t.get_date()
+        elif t.get_date() < self.latest_date:
+            raise TransactionSequenceError(self.latest_date)
+
+        balance_ok = self._check_balance(t)
+        if balance_ok == False and not t.is_exempt():
+            raise OverdrawError
+        limits_ok = self._check_limits(t)
+        if limits_ok == False:
+            raise TransactionLimitError
+
+        if t.is_exempt() or (balance_ok and limits_ok):
+            self._transactions.append(t)
+            self.last_exempt = t._exempt
+            self.latest_date = t.get_date()
+            logging.debug(f"Created transaction: {self._account_number}, {Decimal(amt)}")
+
+    def _check_balance(self, t):
+        """Checks whether an incoming transaction would overdraw the account
+
+        Args:
+            t (Transaction): pending transaction
+
+        Returns:
+            bool: false if account is overdrawn
+        """
+        return t.check_balance(self.get_balance())
+
+    def _check_limits(self, t):
+        return True
+
+    def get_balance(self):
+        """Gets the balance for an account by summing its transactions
+
+        Returns:
+            Decimal: current balance
+        """
+        # could have a balance variable updated when transactions are added (or removed) which is faster
+        # but this is more foolproof since it's always in sync with transactions
+        return sum(x for x in self._transactions)
+
+    def _interest(self):
+        """Calculates interest for an account balance and adds it as a new transaction exempt from limits.
+        """        
+        last_day_in_month = calendar.monthrange(self.latest_date.year, self.latest_date.month)[1]
+        new_date = self.latest_date.replace(day=last_day_in_month)
+        self.add_transaction(self.get_balance() * self._interest_rate, date=str(new_date), exempt=True)
+
+    def _fees(self):
+        pass
+
+    def assess_interest_and_fees(self):
+        "Trigger interest and fees calculation for this account"
+        if self.last_exempt:
+            raise TransactionSequenceError(self.latest_date)
+        self._interest()
+        self._fees()
+
+
+
+    def __str__(self):
+        """Formats the account number and balance of the account.
+        For example, '#000000001,<tab>balance: $50.00'
+        """    
+        return f"#{self._account_number:09},\tbalance: ${self.get_balance():,.2f}"
+
+    def get_transactions(self):
+        return sorted(self._transactions)
+
+class SavingsAccount(Account):
+    """Concrete Account class with daily and monthly account limits and high interest rate.
+    """    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._interest_rate = Decimal("0.025")
+        self._daily_limit = 2
+        self._monthly_limit = 5
+
+    def _check_limits(self, t1:Transaction) -> bool:
+        """determines if the incoming trasaction is within the accounts transaction limits
+
+        Args:
+            t1 (Transaction): pending transaction to be checked
+
+        Returns:
+            bool: true if within limits and false if beyond limits
+        """    
+        num_today = len(
+            [t2 for t2 in self._transactions if not t2.is_exempt() and t2.in_same_day(t1)])
+        num_this_month = len(
+            [t2 for t2 in self._transactions if not t2.is_exempt() and t2.in_same_month(t1)])
+        return num_today < self._daily_limit and num_this_month < self._monthly_limit
+
+    def __str__(self):
+        """Formats the type, account number, and balance of the account.
+        For example, 'Savings#000000001,<tab>balance: $50.00'
+        """ 
+        return "Savings" + super().__str__()
+
+
+class CheckingAccount(Account):
+    """Concrete Account class with lower interest rate and low balance fees.
+    """  
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._interest_rate = Decimal("0.0015")
+        self._balance_threshold = 100
+        self._low_balance_fee = -10
+
+    def _fees(self):
+        """Adds a low balance fee if balance is below a particular threshold. Fee amount and balance threshold are defined on the CheckingAccount.
+        """
+        last_day_in_month = calendar.monthrange(self.latest_date.year, self.latest_date.month)[1]
+        new_date = self.latest_date.replace(day=last_day_in_month)
+
+        if self.get_balance() < self._balance_threshold:
+            self.add_transaction(self._low_balance_fee, date=str(new_date), exempt=True)
+
+    def __str__(self):
+        """Formats the type, account number, and balance of the account.
+        For example, 'Checking#000000001,<tab>balance: $50.00'
+        """ 
+        return "Checking" + super().__str__()
